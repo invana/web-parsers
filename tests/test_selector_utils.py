90c3a731639b16a3fdd6099241ba0ff193aa50f7
Ravi Raja Merugu <rrmerugu@gmail.com>
2020-03-12 00:47:08 +0530

    selector util tests updated

--- tests/test_selector_utils.py
+++ tests/test_selector_utils.py
@@ -1,4 +1,4 @@
-from web_parser.utils.selectors import convert_html_to_selector
+from web_parser.utils.selectors import convert_html_to_selector, transform_data
 import os
 from parsel import Selector
 
@@ -8,3 +8,10 @@ def test_convert_html_to_selector():
     html = open("{}/tests/page.html".format(path), "r").read()
     a = convert_html_to_selector(html)
     assert type(a) is Selector
+
+
+def test_transform_data():
+    a = transform_data(data="1", data_type="IntField")
+    assert type(a) is int
+    a = transform_data(data="1.1", data_type="FloatField")
+    assert type(a) is float
