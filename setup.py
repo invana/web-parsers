cdaeac0f8733dbf3faf65ab398070b8911c68df9
Ravi Raja Merugu <rrmerugu@gmail.com>
2020-03-13 12:29:32 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='extraction-parsers',
-    version='0.0.2',
+    name='web-parsers',
+    version='0.0.3',
     description='Extract data as json from html web pages using YAML configurations',
     author='Ravi Raja Merugu',
     author_email='ravi@invanalabs.ai',
-    url='https://github.com/crawlerflow/extraction-engine',
+    url='https://github.com/invanalabs/web-parser',
     packages=find_packages(
-        exclude=("dist", "docs", "examples", "tests",)
+        exclude=("dist", "docs", "tests",)
     ),
     install_requires=[
         'parsel>=1.5.2',
