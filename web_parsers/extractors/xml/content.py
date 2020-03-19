d776e163f14073941896cfc3af9c2636fee72d42
Ravi Raja Merugu <rrmerugu@gmail.com>
2020-03-19 13:53:35 +0530

    xml field flatten data implemented

--- web_parsers/extractors/xml/content.py
+++ web_parsers/extractors/xml/content.py
@@ -3,16 +3,17 @@ from web_parsers.utils.xml import extract_xml_field, get_nodes
 
 
 class CustomDataExtractor(ContentExtractorBase):
+    extracted_data = {}
 
-    @staticmethod
-    def flatten_data(data=None):
-        if data is None:
-            return data
-        return data.values[0]
+    def add_to_field(self, field_data=None, extractor_field=None):
+        if extractor_field.flatten_data:
+            if field_data is not None:
+                self.extracted_data.update(field_data)
+        else:
+            self.extracted_data[extractor_field.field_id] = field_data
 
     def run(self):
         extractor_fields = self.extractor.extractor_fields
-        extracted_data = {}
         for extractor_field in extractor_fields:
             nodes = get_nodes(
                 self.html_selector,
@@ -29,14 +30,15 @@ class CustomDataExtractor(ContentExtractorBase):
                                 node, child_extractor
                             )
                         extracted_items.append(extracted_item)
-                    extracted_data[extractor_field.field_id] = extracted_items
+                    self.add_to_field(field_data=extracted_items, extractor_field=extractor_field)
                 else:
                     extracted_items = []
                     for node in nodes:
                         extracted_items.append(extract_xml_field(
                             node, extractor_field
                         ))
-                    extracted_data[extractor_field.field_id] = extracted_items
+                    self.add_to_field(field_data=extracted_items, extractor_field=extractor_field)
+
             else:
                 if extractor_field.data_attribute == "element":
                     node = nodes[0]
@@ -45,9 +47,12 @@ class CustomDataExtractor(ContentExtractorBase):
                         extracted_item[child_extractor.field_id] = extract_xml_field(
                             node, child_extractor
                         )
-                    extracted_data[extractor_field.field_id] = extracted_item
+                    self.add_to_field(field_data=extracted_item, extractor_field=extractor_field)
+
                 else:
-                    extracted_data[extractor_field.field_id] = extract_xml_field(
+                    field_data = extract_xml_field(
                         self.html_selector, extractor_field
                     )
-        return extracted_data
+                    self.add_to_field(field_data=field_data, extractor_field=extractor_field)
+
+        return self.extracted_data
