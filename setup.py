693f0259982d77b796427b66e6a9e8a73f1385af
Ravi Raja Merugu <rrmerugu@gmail.com>
2020-03-20 21:09:35 +0530

    setup.py updated with classifier information

--- setup.py
+++ setup.py
@@ -20,4 +20,24 @@ setup(
     ],
     entry_points={
     },
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Environment :: Console',
+        'Intended Audience :: Developers',  # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Apache License',  # Again, pick a license
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Mac OS',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
